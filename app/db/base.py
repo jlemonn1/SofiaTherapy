22277933c2e2520e5f3c8010391bc9661f548b43
jlemonn1 <javierlimon6@gmail.com>
2025-05-03 14:07:19 +0200

    añadido en base la conf de mysql railway

--- app/db/base.py
+++ app/db/base.py
@@ -6,10 +6,10 @@ import os
 load_dotenv()
 
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASS = os.getenv("DB_PASS", "example")
-DB_HOST = os.getenv("DB_HOST", "db")  # ← IMPORTANTE: se refiere al servicio `db` de docker-compose
+DB_PASS = os.getenv("DB_PASS", "mcsAnYkdtkHLLxkESzMmuhkTqcgZMaGL")
+DB_HOST = os.getenv("DB_HOST", "mysql.railway.internal")  # ← IMPORTANTE: se refiere al servicio `db` de docker-compose
 DB_PORT = os.getenv("DB_PORT", "3306")
-DB_NAME = os.getenv("DB_NAME", "sofia")
+DB_NAME = os.getenv("DB_NAME", "railway")
 
 DATABASE_URL = (
     f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
